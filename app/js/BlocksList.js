4cf171c996d28545c1ba3a19a69b959077495c2d
syldium <syldium@mailo.com>
2020-07-07 12:57:51 +0200

    Update minecraft-data

--- app/js/BlocksList.js
+++ app/js/BlocksList.js
@@ -5,7 +5,7 @@ export async function getBlocksList(dataBridge)
     if (cache.length !== 0) {
         return new Promise((resolve) => resolve(cache));
     }
-    cache = await fetch(`https://unpkg.com/minecraft-data@2.51.0/minecraft-data/data/pc/${dataBridge.serverVersion}/blocks.json`, {
+    cache = await fetch(`https://unpkg.com/minecraft-data@2.62.1/minecraft-data/data/pc/${dataBridge.serverVersion}/blocks.json`, {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
